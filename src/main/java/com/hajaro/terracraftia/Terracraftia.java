8803d93b313fc8558839a43d1819fc8f98e9203e
Hajaro <Hajaro@users.noreply.github.com>
2020-08-13 15:09:11 +0200

    Added Ruby Block

--- src/main/java/com/hajaro/terracraftia/Terracraftia.java
+++ src/main/java/com/hajaro/terracraftia/Terracraftia.java
@@ -1,6 +1,8 @@
 package com.hajaro.terracraftia;
 
 import com.hajaro.terracraftia.util.RegistryHandler;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -8,8 +10,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("terracraftia")
@@ -34,6 +34,13 @@ public class Terracraftia
     private void doClientStuff(final FMLClientSetupEvent event) {
 
     }
+
+    public static final ItemGroup TAB = new ItemGroup("TerracraftiaTab") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(RegistryHandler.RUBY.get());
+        }
+    };
 
 
 }
--- src/main/java/com/hajaro/terracraftia/items/ItemBase.java
+++ src/main/java/com/hajaro/terracraftia/items/ItemBase.java
@@ -1,11 +1,12 @@
 package com.hajaro.terracraftia.items;
 
+import com.hajaro.terracraftia.Terracraftia;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
 public class ItemBase extends Item {
 
     public ItemBase(){
-        super(new Item.Properties().group(ItemGroup.MATERIALS));
+        super(new Item.Properties().group(Terracraftia.TAB));
     }
 }
